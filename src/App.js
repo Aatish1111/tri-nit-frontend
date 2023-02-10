584c67bedc5897b6c72c1ebeb1423fdcc51d12eb
Pritam kumar dash <pritamkumar72002@gmail.com>
2023-02-11 04:29:11 +0530

    sass added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { Route, Routes, Navigate } from "react-router-dom";
 import Main from "./Main/index";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
+import Signup from "./pages/Signup"
+import Login from "./pages/Login";
 
 function App() {
 	const user = localStorage.getItem("token");
