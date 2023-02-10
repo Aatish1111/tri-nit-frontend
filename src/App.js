ea718be5c423b1f6a9cc1f61d8afa56414b7b104
Pritam kumar dash <pritamkumar72002@gmail.com>
2023-02-11 05:28:50 +0530

    responsive navbar added and look the navbar css part

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Route, Routes, Navigate } from "react-router-dom";
-import Main from "./Main/index";
-import Signup from "./pages/Signup"
+import HomePage from "./pages/Home";
+import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
 function App() {
@@ -9,7 +9,8 @@ function App() {
 
 	return (
 		<Routes>
-			{user && <Route path="/" exact element={<Main />} />}
+			{/* {user && <Route path="/" exact element={<HomePage />} />} */}
+			<Route path="/" exact element={<HomePage />} />
 			<Route path="/signup" exact element={<Signup />} />
 			<Route path="/login" exact element={<Login />} />
 			<Route path="/" element={<Navigate replace to="/login" />} />
