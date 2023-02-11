214f8e22ac453b8e7a49e97aa0576383f06a0038
Aatish1111 <aatishkumarmoharana@gmail.com>
2023-02-12 00:38:05 +0530

    Ngo commit

---
+++ src/components/Ngo/Slide1.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const Slide1 = () => {
+    return (
+        <div>
+            <div className='News_Update-card'>
+                <div className='News_Update-left'>
+                    <img src="" alt="" />
+                </div>
+                <div className='News_Update-right'>
+                    <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit</h6>
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque?</h2>
+                </div>
+            </div>
+            <div className='News_Update-card'>
+                <div className='News_Update-left'>
+                    <img src="" alt="" />
+                </div>
+                <div className='News_Update-right'>
+                    <h6>Lorem, ipsum dolor sit amet consectetur adipisicing elit</h6>
+                    <h2>Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque?</h2>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Slide1
